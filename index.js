91718180c34e731288d70d53fe06f505ef3dc61d
Dan Hedgecock <hedgecock.d@gmail.com>
2019-05-27 08:11:35 -0700

    Chore: More fixing
    closes #11

--- index.js
+++ index.js
@@ -1,47 +1,17 @@
 'use strict'
 
-const path = require('path')
-// eslint-disable-next-line import/no-dynamic-require
-const packageJSON = require(path.resolve(process.cwd(), './package.json'))
-
-const { changelogWriterConfigs } = require('./lib/changelog-writer-configs')
-
-const changelogTitle = `# ${packageJSON.name} changelog
-
-This project strictly adheres to semver and will err on the side of releasing majors when
-changes could possibly introduce breaking changes. This changelog is dynamically generated
-with [Semantic Release](https://semantic-release.gitbook.io/semantic-release/) configured
-with [@crystal-ball/semantic-release-base](https://github.com/crystal-ball/semantic-release-base).
-
-> Changelog tags
->
-> - 💥 - Breaking change
-> - 🔖 - Release notes
-> - 💖 - New feature
-> - ✨ - Updates
-> - 🛠 - Fixes
-`
-
-// Update release notes parser options to add ability to include release notes
-// in a commit with keyword `RELEASE NOTES`
-const changelogParserConfigs = { noteKeywords: ['BREAKING CHANGE', 'RELEASE NOTES'] }
+const { changelogTitle } = require('./lib/changelog-title')
+const { writerOpts, parserOpts } = require('./lib/release-notes-generator-opts')
 
 module.exports = {
-  // branch: 'master',
-  branch: 'feat/formatting',
+  branch: 'master',
   // Option is passed to all plugins to configure using ESLint commit format
   preset: 'eslint',
   plugins: [
     '@semantic-release/commit-analyzer',
-    [
-      '@semantic-release/release-notes-generator',
-      {
-        parserOpts: changelogParserConfigs,
-        writerOpts: changelogWriterConfigs,
-      },
-    ],
+    ['@semantic-release/release-notes-generator', { parserOpts, writerOpts }],
     ['@semantic-release/changelog', { changelogTitle }],
-    // ⚠️ Required to create release notes and changelog updates before git+npm tasks
+    // ⚠️ Release notes and changelog must be created before running git+npm tasks
     '@semantic-release/git',
     '@semantic-release/npm',
     '@semantic-release/github',
